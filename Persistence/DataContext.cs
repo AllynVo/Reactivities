0aff47f8046d47fb7eb82d950a02e3b9d039a83f
Allyn <allynvo@yahoo.com>
2020-07-28 14:06:45 -0700

    Iniitial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value {Id = 1, Name = "Allyn"},
+                new Value {Id = 2, Name = "Kevin"},
+                new Value {Id = 3, Name = "Lyly"}
+            );
+        }
+    }
+}
